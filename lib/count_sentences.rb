8a643575dbe4f16c7abe13af38df2282e94269f1
Aman Michael <aman.david.michael@gmail.com>
2020-03-12 05:00:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_width?(".")
   end
 
   def question?
-
+    self.end_width?("?")
   end
 
   def exclamation?
-
+    self.end_width?("!")
   end
 
   def count_sentences
-
+    self.count_sentences
   end
 end
